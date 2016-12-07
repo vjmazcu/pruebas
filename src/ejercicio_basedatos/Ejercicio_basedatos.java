b7b0b7fbf7d143d45bb9eda31f0f735b579562f5
W7Entornos <W7Entornos@10.0.2.15>
2016-12-07 10:26:47 +0100

    Nuevo

---
+++ src/ejercicio_basedatos/Ejercicio_basedatos.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejercicio_basedatos;
+
+import java.sql.SQLException;
+
+
+/**
+ *
+ * @author FranS
+ */
+public class Ejercicio_basedatos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws SQLException {
+        Menu men = new Menu();
+        men.setVisible(true);
+    }
+    
+}
